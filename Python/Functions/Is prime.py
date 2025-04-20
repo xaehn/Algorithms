6e51c8f8eb6a15a90f783357b6a2cfec14580314
xaehn <xaehn0@gmail.com>
2025-04-20 10:08:45 +0900

    fix typo
    fix typo.

--- Python/Functions/Is prime.py
+++ Python/Functions/Is prime.py
@@ -4,7 +4,7 @@ def is_prime(number):
     if number < 2:
         return False
 
-    for divisor in range(1, math.floor(number ** 0.5) + 1):
+    for divisor in range(2, math.floor(number ** 0.5) + 1):
         if number % divisor == 0:
             return False
 
