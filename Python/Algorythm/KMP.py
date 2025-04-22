d50e536c8036b7d473ec1648b06d11f59e64e62d
xaehn <xaehn0@gmail.com>
2025-04-22 20:45:21 +0900

    add algorythm using python.
    add function using python about kmp.

---
+++ Python/Algorythm/KMP.py
@@ -0,0 +1,33 @@
+def kmp_table(pattern):
+    length = len(pattern)
+    table = length * [0]
+
+    prefix = 0
+    for suffix in range(1, length):
+        while 0 < prefix and pattern[prefix] != pattern[suffix]:
+            prefix = table[prefix - 1]
+
+        if pattern[prefix] == pattern[suffix] :
+            prefix += 1
+            table[suffix] = prefix
+
+    return table
+
+def kmp(string, pattern):
+    table = kmp_table(pattern)
+    length = len(string)
+    plength = len(pattern)
+    results = []
+    pidx = 0
+    for idx in range(length):
+        while 0 < pidx and string[idx] != pattern[pidx]:
+            pidx = table[pidx - 1]
+
+        if string[idx] == pattern[pidx]:
+            if pidx == plength - 1:
+                results.append(idx - plength + 1)
+                pidx = table[pidx]
+            else:
+                pidx += 1
+
+    return results
