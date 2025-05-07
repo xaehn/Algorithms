8a0e5f43f6e252744e8ea5d63bb5a821ca0e6aa8
xaehn <xaehn0@gmail.com>
2025-05-07 23:44:34 +0900

    add data structure using python
    add data structure using python about undirected graph, directed graph, weighted undirected graph and weighted directed graph.
    #*.py files are editing.

---
+++ Python/Data Structure/Graph/Directed graph.py
@@ -0,0 +1,64 @@
+from collections import deque
+
+class DirectedGraph:
+    def __init__(self):
+        self.vertices = {}
+        self.edges = {}
+
+    def adjacent_vertices(self, key):
+        return list(self.edges.get(key, []))
+
+    def bfs(self, start_key):
+        visited = set()
+        queue = deque()
+        result = []
+        queue.append(self.vertices[start_key])
+        visited.add(self.vertices[start_key])
+        while queue:
+            vertex = queue.popleft()
+            result.append(vertex)
+            for adjacent_vertex in self.edges[vertex]:
+                if not adjacent_vertex in visited:
+                    visited.add(adjacent_vertex)
+                    queue.append(adjacent_vertex)
+
+        return result
+
+    def clear(self):
+        self.vertices.clear()
+        self.edges.clear()
+
+    def delete_edge(self, u, v):
+        self.edges[u].discard(v)
+
+    def delete_vertex(self, key):
+        for edge in self.edges.values():
+            edge.discard(key)
+
+        del self.edges[key]
+        del self.vertices[key]
+
+    def dfs(self, start_key):
+        visited = set()
+        result = []
+
+        def _dfs(vertex):
+            visited.add(vertex)
+            result.append(vertex)
+            for adjacent_vertex in self.edges[vertex]:
+                if adjacent_vertex not in visited:
+                    _dfs(adjacent_vertex)
+
+        _dfs(self.vertices[start_key])
+
+        return result
+
+    def insert_edge(self, u, v):
+        self.edges[u].add(v)
+
+    def insert_vertex(self, key):
+        self.vertices[key] = key
+        self.edges[key] = set()
+
+    def is_empty(self):
+        return not self.vertices
